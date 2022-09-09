171d7f872b786a6d70f68af3823a3435db77be69
Aleksey Gusev <audetv@gmail.com>
2022-09-09 22:42:08 +0300

    Added login action

--- controllers/authController.go
+++ controllers/authController.go
@@ -26,3 +26,32 @@ func Register(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user entities.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"]))
+	if err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	return c.JSON(user)
+}
