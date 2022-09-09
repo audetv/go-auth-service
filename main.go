36606e0a2f72c370f6d598b8cefee1548b1b811b
Aleksey Gusev <audetv@gmail.com>
2022-09-09 20:51:44 +0300

    Added example gorm connection

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:verysecret@tcp(localhost:33061)/yt_go_auth?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
