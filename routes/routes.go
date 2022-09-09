b91842a20b83278029ca6e94cb3f22b0d541d3fd
Aleksey Gusev <audetv@gmail.com>
2022-09-09 21:02:14 +0300

    Restructure, added controllers package

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/audetv/go-auth-service/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Setup(app *fiber.App) {
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", controllers.Hello)
 }
