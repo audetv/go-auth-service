7d4c7a6f47f6821480b801a92b98b37feca15824
Aleksey Gusev <audetv@gmail.com>
2022-09-09 22:05:12 +0300

    Added password hasher

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/audetv/go-auth-service/entities"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -9,5 +13,12 @@ func Register(c *fiber.Ctx) error {
 		return err
 	}
 
-	return c.JSON(data)
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := entities.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	return c.JSON(user)
 }
