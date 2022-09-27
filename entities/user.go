ac6076733630fbd75251f0f713487d4230acd6ef
Aleksey Gusev <audetv@gmail.com>
2022-09-27 13:07:18 +0300

    Added times to the user struct

--- entities/user.go
+++ entities/user.go
@@ -1,8 +1,12 @@
 package entities
 
+import "time"
+
 type User struct {
-	Id       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	Id        uint   `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  []byte `json:"-"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
